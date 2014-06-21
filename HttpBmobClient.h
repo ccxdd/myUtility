59836c54fe976f2d4f19e8a45a483c86e66faf81
ccxdd <ccxdd@me.com>
2014-06-21 15:33:15 +0800

    Bmob Client

---
+++ HttpBmobClient.h
@@ -0,0 +1,13 @@
+//
+//  HttpBmobClient.h
+//  WenStore
+//
+//  Created by ccxdd on 14-6-21.
+//  Copyright (c) 2014年 ccxdd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpBmobClient : NSObject
+
+@end
---
+++ HttpBmobClient.m
@@ -0,0 +1,13 @@
+//
+//  HttpBmobClient.m
+//  WenStore
+//
+//  Created by ccxdd on 14-6-21.
+//  Copyright (c) 2014年 ccxdd. All rights reserved.
+//
+
+#import "HttpBmobClient.h"
+
+@implementation HttpBmobClient
+
+@end

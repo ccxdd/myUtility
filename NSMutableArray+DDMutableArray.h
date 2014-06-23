1a830a9ec5aa9921f06f3f1331f0fb57f1e21b51
ccxdd <ccxdd@me.com>
2014-06-23 08:15:56 +0800

    NSMutableArray Category

---
+++ NSMutableArray+DDMutableArray.h
@@ -0,0 +1,15 @@
+//
+//  NSMutableArray+DDMutableArray.h
+//  WenStore
+//
+//  Created by ccxdd on 14-6-22.
+//  Copyright (c) 2014年 ccxdd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSMutableArray (DDMutableArray)
+
+- (void)addUniqueObject:(id)object;
+
+@end
---
+++ NSMutableArray+DDMutableArray.m
@@ -0,0 +1,18 @@
+//
+//  NSMutableArray+DDMutableArray.m
+//  WenStore
+//
+//  Created by ccxdd on 14-6-22.
+//  Copyright (c) 2014年 ccxdd. All rights reserved.
+//
+
+#import "NSMutableArray+DDMutableArray.h"
+
+@implementation NSMutableArray (DDMutableArray)
+
+- (void)addUniqueObject:(id)object
+{
+    
+}
+
+@end

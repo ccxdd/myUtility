484c0d7bbb06e712547fa98dfb9962860cb8ee62
ccxdd <ccxdd@me.com>
2014-07-13 00:46:31 +0800

    过滤字典数据

--- NSDictionary+JSONString.h
+++ NSDictionary+JSONString.h
@@ -14,4 +14,14 @@
 
 - (NSData *)JsonUTF8Data;
 
+/**
+ *  过滤字典数据
+ *
+ *  @param keys 键
+ *  @param non  非
+ *
+ *  @return 字典
+ */
+- (instancetype)filterWithKeys:(NSArray *)keys non:(BOOL)non;
+
 @end
--- NSDictionary+JSONString.m
+++ NSDictionary+JSONString.m
@@ -32,4 +32,21 @@
     return [[self JSONString] dataUsingEncoding:NSUTF8StringEncoding];
 }
 
+- (instancetype)filterWithKeys:(NSArray *)keys non:(BOOL)non
+{
+    NSMutableDictionary *dict;
+    
+    if (!non) {
+        dict = [NSMutableDictionary dictionary];
+        [keys enumerateObjectsUsingBlock:^(id key, NSUInteger idx, BOOL *stop) {
+            [dict setValue:self[key] forKey:key];
+        }];
+    } else {
+        dict = [NSMutableDictionary dictionaryWithDictionary:self];
+        [dict removeObjectsForKeys:keys];
+    }
+    
+    return dict;
+}
+
 @end

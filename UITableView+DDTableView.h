90cc8d214303a937fd3fa4c0609ddf0066ef3452
ccxdd <ccxdd@me.com>
2014-07-20 17:19:34 +0800

    多选row，单选row，选中指定row

---
+++ UITableView+DDTableView.h
@@ -0,0 +1,53 @@
+//
+//  UITableView+DDTableView.h
+//  WenStore
+//
+//  Created by ccxdd on 14-7-19.
+//  Copyright (c) 2014年 ccxdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DDDataSource;
+
+@interface UITableView (DDTableView)
+
+/**
+ *  返回当前tableView的DataSource
+ *
+ *  @return DDDateSource
+ */
+- (DDDataSource *)tableViewDS;
+
+/**
+ *  返回当前选中的row值
+ *
+ *  @param sectionKey sectionKey
+ *  @param rowKey     rowKey
+ *
+ *  @return row值
+ */
+- (id)selectedRowValueWithSectionKey:(NSString *)sectionKey rowKey:(NSString *)rowKey;
+
+/**
+ *  返回当前选中的row值(多选)
+ *
+ *  @param sectionKey sectionKey
+ *  @param rowKey     rowKey
+ *
+ *  @return row值(多选)
+ */
+- (NSMutableArray *)selectedRowsValuesWithSectionKey:(NSString *)sectionKey rowKey:(NSString *)rowKey;
+
+/**
+ *  选中指定的row(只支持单一Section)
+ *
+ *  @param array     被选值列表
+ *  @param key       列中的key
+ *  @param targetKey DataSource中的key
+ */
+- (void)selectedRowsForArray:(NSArray *)array key:(NSString *)key targetKey:(NSString *)targetKey;
+
+- (void)setAllowsMultipleSelection:(BOOL)allowsMultipleSelection;
+
+@end
---
+++ UITableView+DDTableView.m
@@ -0,0 +1,88 @@
+//
+//  UITableView+DDTableView.m
+//  WenStore
+//
+//  Created by ccxdd on 14-7-19.
+//  Copyright (c) 2014年 ccxdd. All rights reserved.
+//
+
+#import "UITableView+DDTableView.h"
+#import "DDDataSource.h"
+
+@implementation UITableView (DDTableView)
+
+- (DDDataSource *)tableViewDS
+{
+    id ds = self.dataSource;
+    
+    if ([ds isKindOfClass:[DDDataSource class]]) {
+        return ds;
+    } else {
+        DLog(@"************DataSource is not DDDateSource!!!************");
+        return nil;
+    }
+}
+
+#pragma mark - allowsMultipleSelection
+
+- (void)setAllowsMultipleSelection:(BOOL)allowsMultipleSelection
+{
+    self.allowsMultipleSelection = allowsMultipleSelection;
+    
+    DDDataSource *ds = [self tableViewDS];
+    
+    if (ds) {
+        ds.isAllowEdit = allowsMultipleSelection;
+        self.editing = allowsMultipleSelection;
+    }
+}
+
+#pragma mark - selectedRowValueWithSectionKey:rowKey:
+
+- (id)selectedRowValueWithSectionKey:(NSString *)sectionKey rowKey:(NSString *)rowKey
+{
+    return [[self tableViewDS] itemAtIndexPath:self.indexPathForSelectedRow sectionKey:sectionKey rowKey:rowKey];
+}
+
+#pragma mark - selectedRowsValuesWithSectionKey:rowKey:
+
+- (NSMutableArray *)selectedRowsValuesWithSectionKey:(NSString *)sectionKey rowKey:(NSString *)rowKey
+{
+    DDDataSource *ds = [self tableViewDS];
+    
+    if (ds) {
+        NSArray *indexPaths = self.indexPathsForSelectedRows;
+        NSMutableArray *values = [NSMutableArray array];
+        for (NSIndexPath *indexPath in indexPaths) {
+            [values addObject:[ds itemAtIndexPath:indexPath sectionKey:sectionKey rowKey:rowKey]];
+        }
+        
+        return values;
+    }
+    
+    return nil;
+}
+
+#pragma mark - selectedRowsForArray:key:targetKey:
+
+- (void)selectedRowsForArray:(NSArray *)array key:(NSString *)key targetKey:(NSString *)targetKey
+{
+    DDDataSource *ds = [self tableViewDS];
+    
+    if (ds && self.allowsMultipleSelection) {
+        [array enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            NSString *keyValue = [NSString stringWithFormat:@"%@", key ? obj[key] : obj];
+            [ds.tableData enumerateObjectsUsingBlock:^(id tableDataObj, NSUInteger idx, BOOL *stop) {
+                NSString *targetValue = [NSString stringWithFormat:@"%@", targetKey?tableDataObj[targetKey]:tableDataObj];
+                if ([keyValue isEqualToString:targetValue]) {
+                    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:idx inSection:0];
+                    [self selectRowAtIndexPath:indexPath
+                                      animated:NO
+                                scrollPosition:UITableViewScrollPositionNone];
+                }
+            }];
+        }];
+    }
+}
+
+@end

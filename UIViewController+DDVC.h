cddb5ee9bba3c82f098f38707213eb5743aad0f0
ccxdd <ccxdd@me.com>
2014-07-21 18:09:16 +0800

    移除属性类方法

--- UIViewController+DDVC.h
+++ UIViewController+DDVC.h
@@ -56,32 +56,4 @@
  */
 - (void)popToClass:(NSString *)className;
 
-/**
- *  返回所有属性名
- *
- *  @return 属性列表
- */
-- (NSArray *)getPropertyNames;
-
-/**
- *  返回属性字典
- *
- *  @return 字典
- */
-- (NSMutableDictionary *)getPropertyObjects;
-
-/**
- *  返回所有属性值
- *
- *  @return 字典
- */
-- (NSMutableDictionary *)getPropertyValues;
-
-/**
- *  设置所有属性值
- *
- *  @param values values
- */
-- (void)setPropertyValues:(NSDictionary *)values;
-
 @end
